486af617384c596b5b931262dddd291f7481cf1d
Abhishek65a <atupsundar3@gmail.com>
2024-01-17 17:39:01 +0530

    find and filter diference

---
+++ filter-find.js
@@ -0,0 +1,46 @@
+// filter()
+
+// THIS RETURN THE MATCHED VALUES IN AN ARRAY FROM THE COLLECTION.
+
+// =================================================================
+
+//  find()
+
+//method returns the first value that matches from the collection
+//once it matches the value in finding ,it will not check the remaining
+//values in the array collection.
+
+// ===============================================================
+
+// const empArr = [
+//   { name: "abhishek", age: 20 },
+//   { name: "abhi", age: 27 },
+//   { name: "amit", age: 23 },
+//   { name: "akash", age: 22 },
+//   { name: "anmol", age: 26 },
+//   { name: "chandru", age: 82 },
+//   { name: "honey", age: 28 },
+//   { name: "bunny", age: 25 },
+//   { name: "sunny", age: 72 },
+// ];
+// const filteredItem=empArr.filter((item)=>{
+//     return item.age>30
+// })
+// console.log(filteredItem);
+
+
+const empArr = [
+  { name: "abhishek", age: 20 },
+  { name: "abhi", age: 27 },
+  { name: "amit", age: 23 },
+  { name: "akash", age: 22 },
+  { name: "anmol", age: 26 },
+  { name: "chandru", age: 82 },
+  { name: "honey", age: 28 },
+  { name: "bunny", age: 25 },
+  { name: "sunny", age: 72 },
+];
+const findItem=empArr.find((item)=>{
+    return item.age>30
+})
+console.log(findItem);
