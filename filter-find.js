cb58ec81c617b978ab505d99eef24846265d4cb9
Abhishek65a <atupsundar3@gmail.com>
2024-01-17 17:40:46 +0530

    find and filter diference

--- filter-find.js
+++ filter-find.js
@@ -29,6 +29,23 @@
 // console.log(filteredItem);
 
 
+// const empArr = [
+//   { name: "abhishek", age: 20 },
+//   { name: "abhi", age: 27 },
+//   { name: "amit", age: 23 },
+//   { name: "akash", age: 22 },
+//   { name: "anmol", age: 26 },
+//   { name: "chandru", age: 82 },
+//   { name: "honey", age: 28 },
+//   { name: "bunny", age: 25 },
+//   { name: "sunny", age: 72 },
+// ];
+// const findItem=empArr.find((item)=>{
+//     return item.age>30
+// })
+// console.log(findItem);
+
+
 const empArr = [
   { name: "abhishek", age: 20 },
   { name: "abhi", age: 27 },
@@ -40,7 +57,7 @@ const empArr = [
   { name: "bunny", age: 25 },
   { name: "sunny", age: 72 },
 ];
-const findItem=empArr.find((item)=>{
+const findItem=empArr.map((item)=>{
     return item.age>30
 })
 console.log(findItem);
