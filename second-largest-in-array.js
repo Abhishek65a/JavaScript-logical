6fdeafe7c20e97efc82e4736a8cd91e96a59c5ab
Abhishek65a <atupsundar3@gmail.com>
2024-01-19 14:55:37 +0530

    second largest and third largest

--- second-largest-in-array.js
+++ second-largest-in-array.js
@@ -1,14 +1,26 @@
+//third largest number
+
+// const intArray=[2,8,4,7,9]
+// const largestValue=(arr)=>{
+//     firstLargestValue=Math.max(...arr)
+//     index=arr.indexOf(firstLargestValue)
+//     arr.splice(index,1)
+//     secondLargestValue=Math.max(...arr)
+//     index=arr.indexOf(secondLargestValue)
+//     arr.splice(index,1)
+//     thirdLargestValue=Math.max(...arr)
+//     return(thirdLargestValue)
+// }
+// console.log(largestValue(intArray));
 
 
+//second largest number
 const intArray=[2,8,4,7,9]
 const largestValue=(arr)=>{
     firstLargestValue=Math.max(...arr)
     index=arr.indexOf(firstLargestValue)
     arr.splice(index,1)
     secondLargestValue=Math.max(...arr)
-    index=arr.indexOf(secondLargestValue)
-    arr.splice(index,1)
-    thirdLargestValue=Math.max(...arr)
-    return(thirdLargestValue)
+    return(secondLargestValue)
 }
 console.log(largestValue(intArray));
