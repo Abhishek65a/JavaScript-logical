f604146ed2160893b8738b0dcfec494b800cb830
Abhishek65a <atupsundar3@gmail.com>
2024-01-20 22:58:10 +0530

    missing-number-in-array

---
+++ missing-number-in-array.js
@@ -0,0 +1,16 @@
+// missing-number-in-array
+
+let arrNumber=[1,2,3,4,5,6,9,10]
+let missArray=[]
+const missingValue=(arr)=>{
+    const minValue=Math.min(...arr)
+    const maxValue=Math.max(...arr)
+    for(let i=minValue;i<maxValue;i++)
+    {
+        if(arr.indexOf(i)<0){
+            missArray.push(i)
+        }
+    }
+    return(missArray)
+}
+console.log(missingValue(arrNumber));
