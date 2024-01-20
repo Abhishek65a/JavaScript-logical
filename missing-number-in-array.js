e3fd8beda2f6ba1c997637c066a715007b3eb31b
Abhishek65a <atupsundar3@gmail.com>
2024-01-20 22:59:38 +0530

    missing-number-in-array

--- missing-number-in-array.js
+++ missing-number-in-array.js
@@ -1,6 +1,6 @@
 // missing-number-in-array
 
-let arrNumber=[1,2,3,4,5,6,9,10]
+let arrNumber=[1,2,3,4,5,6,9,10,50]
 let missArray=[]
 const missingValue=(arr)=>{
     const minValue=Math.min(...arr)
