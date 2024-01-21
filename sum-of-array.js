ae6f48f3f968fe37a42cdbc5772a933b4508e0f0
Abhishek65a <atupsundar3@gmail.com>
2024-01-21 23:36:17 +0530

    sum-of-an-array

--- sum-of-array.js
+++ sum-of-array.js
@@ -1,10 +1,20 @@
 
 // sum-of-array
 
+// const array=[1,5,8,9,6,7]
+// const sumOfArray=(arr)=>{
+//     return arr.reduce((pre,cur)=>{
+//         return pre+cur
+//     })
+// }
+// console.log(sumOfArray(array));
+
+
+
 const array=[1,5,8,9,6,7]
 const sumOfArray=(arr)=>{
     return arr.reduce((pre,cur)=>{
-        return pre+cur
+        return pre-cur
     })
 }
 console.log(sumOfArray(array));
