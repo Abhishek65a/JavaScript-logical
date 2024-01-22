42cc881202ccb9516a3c3b445c1caf0d2dfea50c
Abhishek65a <atupsundar3@gmail.com>
2024-01-22 22:36:10 +0530

    pattern program

---
+++ pattern.js
@@ -0,0 +1,19 @@
+function printPatter(rows)
+{
+    for(let i=rows;i>=1;i--)
+    {
+        let row=""
+        for (let j = 1; j <=(rows-i) * 2; j++) 
+        {
+           row +=""
+        }
+        for (let k = 1; k <= i; k++) 
+        {
+            row += "*  "
+        }
+        console.log(row);
+
+    }
+
+}
+printPatter(5)
