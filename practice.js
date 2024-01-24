cf6191d3f8434561604b3119d0a5c83f454b98ce
Abhishek65a <atupsundar3@gmail.com>
2024-01-24 20:42:40 +0530

    prefixed names using map method

--- practice.js
+++ practice.js
@@ -1,5 +1,15 @@
-let x=10
-let y=x++
-let z=++y
+// let x=10
+// let y=x++
+// let z=++y
 
-console.log(x+y+z);
+// console.log(x+y+z);
+
+
+
+// ===================================================
+
+let names=["abhi","amogh","ankit","amruta","kartik"]
+
+const prefixedNames=names.map(names=>`name=${names}`)
+
+console.log(prefixedNames);
