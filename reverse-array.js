ccca26da29a900d97d856a825e916cbc55b48718
Abhishek65a <atupsundar3@gmail.com>
2024-01-24 15:30:47 +0530

    reverse a array

---
+++ reverse-array.js
@@ -0,0 +1,13 @@
+
+// let a=['abhi','anil','ashok',"chandana"]
+let a=[10,50,87,98,57]
+
+console.log(a.reverse()); //inbuilt method
+
+
+
+//without using innbuilt method 
+
+// for(var i=a.length-1;i>=0;i--){
+//     console.log(a[i]);
+// }
